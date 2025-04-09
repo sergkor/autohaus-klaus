d6e228ee80edf7bc648874ada21b34cfc8ad3ba3
Sergiy Korniychuk <skorniychuk@sondermind.com>
2025-04-08 21:35:23 -0700

    fix running locally

--- components/CallbackButton.tsx
+++ components/CallbackButton.tsx
@@ -24,7 +24,17 @@ export default function CallbackButton() {
               ×
             </button>
             <h3>Request a Callback</h3>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe_8j4AzIlCXHyWaQEGMJSC8LbHa4Rjh0_KdHorJYYoZJT3nQ/viewform?embedded=true" width="100%" height="600" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <iframe 
+              src="https://docs.google.com/forms/d/e/1FAIpQLSe_8j4AzIlCXHyWaQEGMJSC8LbHa4Rjh0_KdHorJYYoZJT3nQ/viewform?embedded=true"
+              width="100%"
+              height={600}
+              frameBorder={0}
+              marginHeight={0}
+              marginWidth={0}
+              style={{ border: 0 }}
+            >
+              Loading...
+            </iframe>
           </div>
         </div>
       )}
